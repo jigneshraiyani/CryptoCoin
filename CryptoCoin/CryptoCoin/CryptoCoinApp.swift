869739f2e44ea955322580d9bac8ee1b6fbf43fd
Jignesh Raiyani <jignesh.raiyani1@gmail.com>
2024-03-21 09:40:57 +0530

    Added project setup files

---
+++ CryptoCoin/CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Raiyani Jignesh on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
